6d4c568a4f670f531a9e288c222074aa6a19ba41
Litian Zhao <lz527@cornell.edu>
2021-04-16 20:09:52 -0400

    tutorial

--- tutorial.ml
+++ tutorial.ml
@@ -60,11 +60,89 @@ let initial_int_list () =
       ]
   | _ -> []
 
-let tutorial () =
-  print_endline
-    "At the beginning, the dealer, aka the player who play the \n\
-     first hand (player on house) has 14 tiles and the player has 13 \
-     cards."
+let rec tutorial (chapter : int) : unit =
+  if chapter = 0 then
+    let chapter_0 () =
+      ANSITerminal.print_string
+        [ ANSITerminal.red; ANSITerminal.Bold ]
+        "\n\nWelcome to the Mahjong Game Tutorial.\n";
+      print_endline
+        "\n\
+         This tutorial will cover the following information. \n\n\
+        \    To view sequencely, press enter. To skip to a desired \
+         chapter, \n\n\
+        \    enter the index of the chapter and follow by enter.\n\n";
+      Unix.sleep 1;
+      ANSITerminal.print_string [ ANSITerminal.blue ] "1. Basics\n";
+      Unix.sleep 1;
+      ANSITerminal.print_string [ ANSITerminal.green ] "2. Tiles\n";
+      Unix.sleep 1;
+      ANSITerminal.print_string [ ANSITerminal.cyan ] "3. Game\n";
+      Unix.sleep 1;
+      ANSITerminal.print_string [ ANSITerminal.yellow ] "4. Scoring\n";
+      print_endline "Please select from 1 to 4:\n";
+      print_string "> ";
+      match read_line () with
+      | exception End_of_file ->
+          print_endline "Please select from 1 to 4:";
+          print_string "> "
+      | anystring -> (
+          match int_of_string_opt anystring with
+          | None -> tutorial 0
+          | Some int -> tutorial int)
+    in
+    chapter_0 ()
+  else if chapter = 1 then
+    let chapter_1 () =
+      print_endline
+        "Mahjong (Not Mahjong Solitaire!) is a tile-based game that \
+         was developed in China around the 1600s.  ";
+      Unix.sleep 2;
+      print_endline
+        "It is played with four players. Like the card game UNO, the \
+         four players sequencely make moves, and compete to be the  \
+         first one to met the winning goals.";
+      Unix.sleep 2;
+      print_endline
+        "However, in our implementation of the game, you will play \
+         against three AI players to achieve the same goals.";
+      Unix.sleep 2;
+      print_endline
+        "There will be two modes: Easy, where you will play with \
+         amature AIs; and Hard, where you will play with very smart \
+         AIs.";
+      Unix.sleep 2;
+      print_endline
+        "Now, you are good to go to the next Chapter. Press enter to \
+         continue.";
+      print_string "> ";
+      match read_line () with
+      | exception End_of_file ->
+          print_endline "Press enter to continue.";
+          print_string "> "
+      | anything -> tutorial 2
+    in
+    chapter_1 ()
+  else if chapter = 2 then
+    let chapter_2 () =
+      print_endline
+        "The game is based on a set of 144 tiles (like cards) based on \
+         Chinese characters and symbols";
+      Unix.sleep 2;
+      ()
+    in
+    chapter_2 ()
+  else if chapter = 3 then
+    let chapter_3 () =
+      print_endline
+        "At the beginning, the dealer, aka the player who play the \n\
+         first hand (player on house) has 14 tiles and the player has \
+         13 cards.";
+      Unix.sleep 2;
+      ()
+    in
+    chapter_3 ()
+  else if chapter = 4 then ()
+  else ()
 
-;;
-print_endline " "
+let tutorial_start () = tutorial 0
--- tutorial.mli
+++ tutorial.mli
@@ -1,5 +1,8 @@
 (** **Depreciated** initial a list by int **may only be used in test **)
 val initial_int_list : unit -> int list
 
-(** display toturial *)
-val tutorial : unit -> unit
+(** display toturial chapter [int] *)
+val tutorial : int -> unit
+
+(** display toturial from chapter 0 *)
+val tutorial_start : unit -> unit
