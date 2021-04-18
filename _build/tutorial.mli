36a934ae2ae50b1399308334e9f7446d2b04ea18
Litian Zhao <lz527@cornell.edu>
2021-04-18 19:03:29 -0400

    main menu

--- _build/tutorial.mli
+++
@@ -1,8 +0,0 @@
-(** **Depreciated** initial a list by int **may only be used in test **)
-val initial_int_list : unit -> int list
-
-(** display toturial chapter [int] *)
-val tutorial : int -> unit
-
-(** display toturial from chapter 0 *)
-val tutorial_start : unit -> unit
